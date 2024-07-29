6b9ea7a84e81fc91be4309c14c72135c9610ddfe
phuongdt88 <phuongdt2711@gmail.com>
2024-07-29 09:50:30 +0700

    test cookie

---
+++ Assets/Scrips/TestCookies.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using UnityEngine;
+
+public class TestCookies : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start() {
+        CookieContainer container = new CookieContainer();
+    }
+}
